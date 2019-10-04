2e8ca1c70ee4632221bf10dd12675ee0ba2f4e3c
bachir <bachirtari8ster@gmail.com>
2019-10-04 19:05:59 +0200

    removing some comment and un used properties

--- src/main/java/com/storage/s3demo/s3/S3ObjectRepository.java
+++ src/main/java/com/storage/s3demo/s3/S3ObjectRepository.java
@@ -35,7 +35,6 @@ public class S3ObjectRepository {
 	private String region;
 
   @Bean
-//  @ConditionalOnProperty(name = "localaws.enabled", havingValue = "false")
   public AmazonS3 localstackS3Client() {
     return AmazonS3ClientBuilder.standard()
       .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(EndpointRegistrar.getS3Endpoint(), region))
